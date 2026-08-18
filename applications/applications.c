211300aad54975577f126652e5fbd0f645576c6a
Skorpionm <85568270+Skorpionm@users.noreply.github.com>
2026-08-18 18:48:41 +0400

    Add protobuf and gui optimization (#210)
    * Init protobuf
    * NanoPB: generate .h file
    * Cli: add "rpc"
    * Rpc: fix registry rpc_cli_command_start_session
    * ci: install protobuf-compiler in build workflow
    * init rpc_test.py
    * add emulation button
    * add emulate touch
    * fix help rpc_test.py
    * Rpc: add RpcScreenStream
    * Touchpad_test: reduce the number of lines
    * Rpc_screen_stream: refactoring -37 kb use ram
    * Fix close session
    * update .gitignore
    * add --verbose for rpc_test
    * Rpc_test: add interactive mode added
    * Rpc_test: fix run macos
    * Assets: protobuf in .venv
    * Assets: add requirements.txt
    * fix buildbo
    * GUI: reduction in RAM consumption
    * Frame rendering optimization; MCU usage reduced from 37% to 24% while the walkie-talkie application is running.
    * GUI: optimization of rounded corner rendering
    * Driver: add show FPS and display ma[ 80 FPS
    * Gui: displaying the image directly on the screen when there is no need to overlay a menu.
    * i2c_intercom: fix priority
    * Build: test build without protobuf-compiler
    * Gui: fix fast frame
    * Render: fix canary access UB
    * Gui, fast_frame: add TODO
    * Turn off FPS debug
    * Display: 80FPS comment
    
    ---------
    
    Co-authored-by: SG <who.just.the.doctor@gmail.com>

--- applications/applications.c
+++ applications/applications.c
@@ -27,6 +27,7 @@ extern int32_t haptic_test_app(void* p);
 extern int32_t self_check_app(void* p);
 extern int32_t font_test_app(void* p);
 extern int32_t cli_on_system_start(void* p);
+extern int32_t rpc_on_system_start(void* p);
 extern int32_t dmesg_app(void* p);
 extern int32_t unit_test_app(void* p);
 
@@ -94,13 +95,13 @@ const FlipperInternalApplication FLIPPER_SERVICES[] = {
         .stack_size = 1024 * 4,
         .flags = FlipperInternalApplicationFlagDefault,
     },
-    // {
-    //     .app = test_peref_srv,
-    //     .name = "TestPerefSrv",
-    //     .appid = "test_peref_srv",
-    //     .stack_size = 1024,
-    //     .flags = FlipperInternalApplicationFlagDefault,
-    // },
+    {
+        .app = test_peref_srv,
+        .name = "TestPerefSrv",
+        .appid = "test_peref_srv",
+        .stack_size = 1024,
+        .flags = FlipperInternalApplicationFlagDefault,
+    },
     {
         .app = gui_srv,
         .name = "GuiSrv",
@@ -218,6 +219,13 @@ const FlipperInternalApplication FLIPPER_AUTORUN_APPS[] = {
         .app = cli_on_system_start,
         .name = "CliOnSystemStart",
         .appid = "cli_on_system_start",
+        .stack_size = 1024 * 2,
+        .flags = FlipperInternalApplicationFlagDefault,
+    },
+    {
+        .app = rpc_on_system_start,
+        .name = "RpcOnSystemStart",
+        .appid = "rpc_on_system_start",
         .stack_size = 1024 * 2,
         .flags = FlipperInternalApplicationFlagDefault,
     },
